517808376f90fd59e0a5f77d53746735785a58ab
Ian <ianm2927@gmail.com>
2021-11-23 13:04:28 -0500

    V2

--- html/js/code.js
+++ html/js/code.js
@@ -4,57 +4,112 @@ const startBtn = document.getElementById('start_btn');
 const container = document.querySelector('.container');
 const queContainer = document.querySelector(".questions_container")
 let fragment = document.createDocumentFragment();
+let points = 0
 
 startBtn.addEventListener('click',(e)=>{
     hidePre()
     createQuestions()
-    createElements()
+    searchQuestion(preguntas)
+    //createElements()
 })
 
+
+const contador = ()=>{
+
+}
 let preguntas = {
     qu1 :[
-        question = '',
-        res = ''
+        question = '1. País más grande del mundo',
+        res = 'Rusia',
+        options = ["USA","Canadá","Rusia"]
     ], 
     qu2:[
-        question = '',
-        res = ''
+        question = '2. País con más idiomas del mundo',
+        res = 'Papúa Nueva Guinea',
+        options = ["India","Papúa NUeva Guinea","Indonesia"]
     ],
     qu3:[
-        question = '',
-        res = ''
+        question = '3. Idioma más hablado en Bélgica',
+        res = 'Francés',
+        options = ["Francés","Neerlandés","Alemán"]
     ],
     qu4:[
-        question = '',
-        res = ''
+        question = '4. País con el mayor salario mínimo',
+        res = 'Australia',
+        options = ["Usa","Alemania","Australia"]
     ],
     qu5:[
-        question = '',
-        res = ''
+        question = '5. Moneda utilizada en Suiza',
+        res = 'Franco suizo',
+        options = ["Euro","Franco suizo","Corona suiza"]
     ],
     qu6:[
-        question = '',
-        res = ''
+        question = '6. Medio de transporte sostenible',
+        res = 'Bicicleta',
+        options = ["Autos eléctricos","Moto","Bicicleta"]
     ],
     qu7:[
-        question = '',
-        res = ''
+        question = '7. Grupo de lenguas al que pertenece el inglés',
+        res = 'germánicas',
+        options = ["latinas","germánicas","celtas"]
     ],
-     
+    qu8:[
+        question = '8. País más pequeño del mundo',
+        res = 'Nauru',
+        options = ["Nauru","Fidji","Malta"]
+    ],
+    qu9:[
+        question = '9. Grupo étnico predominante en China, Japón, Corea etc.',
+        res = 'mongoloide',
+        options = ["oriental","asiático","mongoloide"]
+    ],
+    qu10:[
+        question = '10. Reino al que pertenecen los hongos ',
+        res = 'Fungi',
+        options = ["Fungi","Vegetal","Monera"]
+    ],
+    
 
     
 }
 
-const createElements = (e)=>{
-    const h2 = document.createElement('h2').classList.add('q_title')
-    const p = document.createElement('p').classList.add('text')
-    const div = document.createElement('div').classList.add('qu_section')
-    fragment.appendChild(h2);
-    fragment.appendChild(p);
-    fragment.appendChild(div);
-    queContainer.appendChild(fragment)  
+let number = 0
+const searchQuestion = (data)=>{
+     let entrie =  Object.entries(data)
+     let datos = entrie.forEach(dat =>{
+         const div = document.createElement('div')
+         const h2 = document.createElement('h2')
+         const resUer = document.createElement('input')
+         resUer.type = 'text'
+         addClasses(h2,'que_title')
+         addClasses(div,'que_section')
+         addClasses(resUer,"res_user")
+         h2.innerText = dat[1][0]
+         creAndSerOptions(dat[1][2],div)
+         div.appendChild(h2);
+         div.appendChild(resUer)
+         fragment.appendChild(div)
+         queContainer.appendChild(fragment)
+        })
+
+        return datos
+    }
+
+const creAndSerOptions = (dat,div)=>{
+    dat.forEach(el =>{
+        let option = document.createElement('option')
+        addClasses(option,"options")
+        option.textContent = `${number} ${el}`;
+        fragment.appendChild(option)
+        div.appendChild(fragment)      
+         })
 }
 
+
+
+function addClasses(el, cla) {
+    el.classList.add(cla);
+}
 
 
 const createQuestions = ()=>{
@@ -66,4 +121,7 @@ const createQuestions = ()=>{
 
 const hidePre = ()=>{
     container.style.opacity = '0';
+    setTimeout(() => {
+        container.style.display = 'none'  
+    }, 1000);
 }
